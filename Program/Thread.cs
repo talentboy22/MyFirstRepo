47f09922dc104ba807f38f325d3950ec59650f8b
Bob <k1935470718@qq.com>
2025-07-03 10:25:24 +0800

    no
    1243

---
+++ Program/Thread.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyFirsttry
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
